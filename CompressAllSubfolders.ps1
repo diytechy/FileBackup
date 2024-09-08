f1879ce0a1105b50262d5b15063b33feb1b32f40
Peter Johnson <peterandersjohnson@outlook.com>
2024-09-08 18:50:33 -0500

    Adding new scripts.

--- CompressAllSubfolders.ps1
+++ CompressAllSubfolders.ps1
@@ -1,10 +1,13 @@
 ﻿# NOTE: Below function copied from stack-overflow:
 #https://stackoverflow.com/questions/43728173/looping-through-all-subfolders-zipping-each-folder-in-powershell
 
-Fldrs2Comp = "E:\02\ToSift\ToSort\Isolate\Server Backups\Pre_MINI-SERV"
-OutputFldr = "E:\02\ToSift\ToSort\Isolate\Server Backups\Pre_MINI-SERV_Comp"
+$Fldrs2Comp = "E:\02\ToSift\ToSort\Isolate\Server Backups\Pre_MINI-SERV"
+$OutputFldr = "E:\02\ToSift\ToSort\Isolate\Server Backups\Pre_MINI-SERV_Comp"
 
 
+
+
+$7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
 if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
     throw "7 zip executable '$7zipPath' not found"
 }
@@ -12,33 +15,14 @@ else {
     Set-Alias Start-SevenZip $7zipPath
 }
 
+$subfolders = Get-ChildItem $Fldrs2Comp | Where-Object { $_.PSIsContainer }
 
-
-function Compress-Subfolders
+ForEach ($s in $subfolders) 
 {
-    param
-    (
-        [Parameter(Mandatory = $true)][string] $InputFolder,
-        [Parameter(Mandatory = $true)][string] $OutputFolder
-    )
-
-    $subfolders = Get-ChildItem $InputFolder | Where-Object { $_.PSIsContainer }
-
-    ForEach ($s in $subfolders) 
-    {
-        $path = $s
-        $path
-        Set-Location $path.FullName
-        $fullpath = $path.FullName
-        $pathName = $path.BaseName
-
-        #Get all items 
-        $items = Get-ChildItem
-
-        $zipname = $path.name + ".zip"
-        $zippath = Join-Path $outputfolder $zipname
-        Compress-Archive -Path $items -DestinationPath $zippath
-        
-        Start-SevenZip a -mx=9 -bso0 -bsp0 $RepPath7Zip $RepPathFldr
-    }
+    $path = $s
+    $fullpath = $path.FullName
+    $fldrname = $path.BaseName
+    $OutName  = Join-Path $OutputFldr $fldrname
+    $z7name   = $OutName + ".7z"
+    Start-SevenZip a -mx=9 $z7name $fullpath
 }
