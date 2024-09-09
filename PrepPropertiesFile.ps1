17659d699005f077e01d5daf6ca3834434daf5b4
Peter Johnson <peterandersjohnson@outlook.com>
2024-09-08 20:14:25 -0500

    Rename

--- PrepPropertiesFile.ps1
+++ PrepPropertiesFile.ps1
@@ -8,6 +8,7 @@
 #5. Create a new app password.
 #Choose Generate.
 #https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwiemvzirciHAxXYhIkEHQO3AjcQFnoECBkQAQ&url=https%3A%2F%2Fknowledge.workspace.google.com%2Fkb%2Fhow-to-create-app-passwords-000009237&usg=AOvVaw0XTi4ejHyhkIASe-Pqircz&opi=89978449
+
 $Secrets = @{
     FromEmail = "fromuser@gmail.com"
     ToEmail = "touser@hotmail.com"
