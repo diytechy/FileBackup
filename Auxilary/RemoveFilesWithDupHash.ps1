81fe223380411da3be1deae6b017db7cd1b8841f
Peter Johnson <peterandersjohnson@outlook.com>
2024-10-01 22:14:16 -0500

    First draft, incomplete

--- Auxilary/RemoveFilesWithDupHash.ps1
+++ Auxilary/RemoveFilesWithDupHash.ps1
@@ -1,1 +1,12 @@
-﻿
+$HashPaths = @(
+"A:\SharedFilesHashTable.csv"
+"A:\PrivateFilesHashTable.csv"
+"A:\NonDocsFilesHashTable.csv"
+)
+foreach ($path in $HashPaths) {
+    if (Test-Path -LiteralPath $path -PathType Leaf) {
+        $HashProps += @(Import-Csv -LiteralPath $path)
+    }
+}
+Write-Host "All hash definitoins imported"
+#$HashProps.Hash
