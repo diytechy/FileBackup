b3b246692d2aace418e0e2aeff492e44b32b8172
Peter Johnson <peterandersjohnson@outlook.com>
2025-11-22 12:26:15 -0600

    Removing build album folder / script set
    This has been moved to a dedicated public repository "https://github.com/AuroLeap/Pictures2VideoSlideShow"

--- BuildAlbum/ExportVideoToFrames.ps1
+++
@@ -1,7 +0,0 @@
-$VideoIn = "D:\SlowMo.mp4"
-$OutDir = "D:\OutVid\"
-if (Test-Path -LiteralPath $OutDir)
-{
-    New-Item -Path $OutDir -ItemType "directory" | Out-Null
-}
-ffmpeg -i input.mp4 -c:v png ($OutDir + "output_frame%04d.png")
--- BuildAlbum/InstallDependencies.bat
+++
@@ -1,3 +0,0 @@
-cd /D "%~dp0"
-pwsh .\InstallDependencies.ps1
-pause
